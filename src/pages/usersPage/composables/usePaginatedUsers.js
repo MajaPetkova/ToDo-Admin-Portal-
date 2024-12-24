20c93154e28c2bfa9cd0fee7ff1af17fbca1be30
Maja Petkova <majapetkova1@yahoo.com>
2024-12-24 23:17:13 +0100

    Synchronize the state with the URL

--- src/pages/usersPage/composables/usePaginatedUsers.js
+++ src/pages/usersPage/composables/usePaginatedUsers.js
@@ -1,16 +1,20 @@
 import { useOffsetPagination, useWindowScroll } from '@vueuse/core';
 import { ref } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
 import { getPaginatedUsers } from '../../../api/usersAPI';
 
 const FIRST_PAGE = 1;
 const PAGE_SIZE = 10;
 
-const { y } = useWindowScroll();
-function scrollToTop() {
-  y.value = 0;
-}
-
 export function usePaginatedUsers() {
+  const router = useRouter();
+  const route = useRoute();
+
+  const { y } = useWindowScroll();
+  function scrollToTop() {
+    y.value = 0;
+  }
+
   const isLoading = ref(true);
   const users = ref([]);
   const total = ref(0);
@@ -36,10 +40,18 @@ export function usePaginatedUsers() {
     onPageChange: loadUsers,
     onPageSizeChange: loadUsers,
   });
+
+  const trackPaginationInUrl = (qParams) => {
+    router.push({ ...route, query: {
+      ...(route.query ?? {}),
+      ...qParams,
+    } });
+  };
   async function loadUsers() {
     scrollToTop();
     // console.log('local', currentPage.value, currentPageSize.value);
     const qParams = { skip: (currentPage.value - 1) * currentPageSize.value, limit: currentPageSize.value };
+    trackPaginationInUrl(qParams);
     const res = await getPaginatedUsers(qParams);
     // console.log(res);
     users.value = res.users;
